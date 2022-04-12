7c3b7c23f2bc6c4c550aa3dbcb0443fc3afa57f0
METALXRAY <papabear.metalxray@gmail.com>
2022-04-12 11:53:28 +0530

    Build 1.5 12042022

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
 	GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const firebaseConfig = {
 	apiKey: "AIzaSyCKC_jzQr1l_Nv5J0HKjRN32ofclT4J0nM",
 	authDomain: "element-clothing-db.firebaseapp.com",
@@ -25,4 +27,41 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log("Error creating the user", error.message);
+		}
+	}
+
+	return userDocRef;
+
+	// if user data exists
+
+	// return userdocref
+	// else if user data doesnt exists
+
+	// create / set the document  with the data from userauth in my collection
+};
